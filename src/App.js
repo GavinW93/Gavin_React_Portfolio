57b52fdadbe6d6b7b6613a1ac2145d13a22d4b5b
Gavin <ekkambyte@gmail.com>
2020-03-04 00:13:26 -0500

    added more to the ui

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import './App.css';
 import Nav from './components/Front_header/Navigator.js'
 import Maincontainer from './components/Main_container/Maincontainer.js'
 import Maincontainer2 from './components/Main_container2/Maincontainer2.js'
+import Maincontainer3 from './components/Main_container3/Maincontainer3.js'
+import Form from './components/form/Form.js'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Nav/>
+      
+       
+        
+      </header>
+      <body>
       <Maincontainer/>
       <Maincontainer2/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Maincontainer3/>
+      <Form/>
+      {/* <Form/> */}
+      
+      </body>
       
     </div>
     
